682f3921dcc5dd87232f6dac5a349a07c026daf2
MassiWR <90767729+MassiWR@users.noreply.github.com>
2021-12-22 02:00:45 +0100

    added css file and some styling

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do to list</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    
+    <section class="container">
+    <form method="post" action="" class="input_form">
+
+    <section class="header">
+        <h1>To do list</h1>
+    </section>
+
+    <input type="text" name="title" class="task_title" placeholder="Title">
+    <input type="text" name="task" class="task_input" placeholder="Task description">
+    <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+    </form>
+    </section>
+
+
+</body>
+</html>
