7dc167938b193853e651aba108facc53f1aa9d75
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-02-01 21:38:13 +0100

    change from database connection from pdo to procedural

--- database/database.php
+++ database/database.php
@@ -3,35 +3,16 @@ if(!defined('__CONFIG__')) {
     header("Location: ../index.php");
 }
 
-class database {
-    protected static $connection;
-    private function __construct() {
-        $host = '127.0.0.1';
-        $db = 'toDoList';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
-        $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
-        try {
-            self::$connection = new PDO($dsn, $user, $pass);
-            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$connection->setAttribute(PDO::ATTR_PERSISTENT, false);
-        }
-        catch(PDOException $event) {
-            echo "Could not connect to database"; 
-            throw new \PDOException($event->getMessage(), (int)$event->getCode());
-            exit;
-            
-        }
-    }
+//Mysql connection config
+$host = '127.0.0.1';
+$user = 'root';
+$pass = '';
+$db = 'todo';
 
-    public static function getConnection() {
-        if(!self::$connection) {
-            new database();
-        }
+$connection = mysqli_connect($hose,$user,$pass,$db);
 
-        return self::$connection;
-    }
+if(!$connection){
+	die('Mysql connection error : '.mysqli_connect_error());
 }
 
 
